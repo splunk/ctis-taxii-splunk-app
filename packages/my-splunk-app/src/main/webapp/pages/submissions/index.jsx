d190e31ab107e8cddad44e11620c481f407fec97
Ben Liew <bliew@splunk.com>
2024-07-16 11:29:35 +1000

    Add submissions page

---
+++ packages/my-splunk-app/src/main/webapp/pages/submissions/index.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import layout from '@splunk/react-page';
+import MyReactComponent from '@splunk/my-react-component';
+import { getUserTheme } from '@splunk/splunk-utils/themes';
+
+import { StyledContainer, StyledGreeting } from './StartStyles';
+
+getUserTheme()
+    .then((theme) => {
+        layout(
+            <StyledContainer>
+                <StyledGreeting>Submissions</StyledGreeting>
+                <div>Your component will appear below.</div>
+                <MyReactComponent name="from inside MyReactComponent" />
+            </StyledContainer>,
+            {
+                theme,
+            }
+        );
+    })
+    .catch((e) => {
+        const errorEl = document.createElement('span');
+        errorEl.innerHTML = e;
+        document.body.appendChild(errorEl);
+    });
