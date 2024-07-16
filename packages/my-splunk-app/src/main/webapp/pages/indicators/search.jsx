d5ca804ce6b52781e91663c2b764488037027e7d
Ben Liew <bliew@splunk.com>
2024-07-16 17:35:04 +1000

    Fixed search bar styling with flexbox

--- packages/my-splunk-app/src/main/webapp/pages/indicators/search.jsx
+++ packages/my-splunk-app/src/main/webapp/pages/indicators/search.jsx
@@ -1,39 +1,46 @@
-// https://splunkui.splunk.com/Packages/react-ui/Search
 import React, {useState} from 'react';
 import Search from '@splunk/react-ui/Search';
 import Select from '@splunk/react-ui/Select';
 import Button from '@splunk/react-ui/Button';
 import Magnifier from '@splunk/react-icons/Magnifier';
+import ButtonGroup from '@splunk/react-ui/ButtonGroup';
+
 
 import styled from 'styled-components';
 
-// TODO: Use flex or grid to organise components horizontally
 const SearchControlContainer = styled.div`
-    display: inline-block;
+    display: flex;
+    gap: 0;
 `;
 
+const StyledButtonGroup = styled(ButtonGroup)`
+    flex: 0.25 1 200px;
+`;
 
 
 // Control group for search: field drop down, search box and submit button
 const SearchControlGroup = () => {
     const [value, setValue] = useState('');
 
-    const handleChange = (e, { value: searchValue }) => {
+    const handleChange = (e, {value: searchValue}) => {
         setValue(searchValue);
     };
 
     return (
         <SearchControlContainer>
-            <Select defaultValue="1">
-                <Select.Option label="Any Field" value="1" />
-                <Select.Option label="Indicator ID" value="2" />
-                <Select.Option label="Splunk Field" value="3" />
-            </Select>
             <Search
                 onChange={handleChange}
                 value={value}
+                style={{flex: 8}}
             />
-            <Button icon={<Magnifier />} label="Search" />
+            <StyledButtonGroup>
+                <Select defaultValue="1">
+                    <Select.Option label="Any Field" value="1"/>
+                    <Select.Option label="Indicator ID" value="2"/>
+                    <Select.Option label="Splunk Field" value="3"/>
+                </Select>
+                <Button icon={<Magnifier/>} appearance="primary" label="Search"/>
+            </StyledButtonGroup>
         </SearchControlContainer>
     );
 }
