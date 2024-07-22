51a00a516b84b2b9185c9bc44ba835e0b1a8df19
Ben Liew <bliew@splunk.com>
2024-07-22 15:32:47 +1000

    DateTime with timezone offset component. Add submit button to new indicator form

---
+++ packages/my-react-component/src/DateTimeWithTimezone.jsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import Select from "@splunk/react-ui/Select";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+    display: flex;
+    width: 100%;
+    gap: 10px;
+`;
+const DatetimeInput = styled.input`
+    flex-basis: 60%;
+    flex-grow: 6;
+`
+const TimezoneDropdown = styled(Select)`
+    flex-basis: 40%;
+    flex-grow: 4;
+`
+const TIMEZONE_UTC_OFFSETS = [
+    {'offset': '-12:00', 'timezones': []},
+    {'offset': '-11:00', 'timezones': []},
+    {'offset': '-10:00', 'timezones': []},
+    {'offset': '-09:30', 'timezones': []},
+    {'offset': '-09:00', 'timezones': []},
+    {'offset': '-08:00', 'timezones': []},
+    {'offset': '-07:00', 'timezones': []},
+    {'offset': '-06:00', 'timezones': []},
+    {'offset': '-05:00', 'timezones': []},
+    {'offset': '-04:00', 'timezones': []},
+    {'offset': '-03:30', 'timezones': []},
+    {'offset': '-03:00', 'timezones': []},
+    {'offset': '-02:00', 'timezones': []},
+    {'offset': '-01:00', 'timezones': []},
+    {'offset': '+00:00', 'timezones': []},
+    {'offset': '+01:00', 'timezones': []},
+    {'offset': '+02:00', 'timezones': []},
+    {'offset': '+03:00', 'timezones': []},
+    {'offset': '+03:30', 'timezones': []},
+    {'offset': '+04:00', 'timezones': []},
+    {'offset': '+04:30', 'timezones': []},
+    {'offset': '+05:00', 'timezones': []},
+    {'offset': '+05:30', 'timezones': []},
+    {'offset': '+05:45', 'timezones': []},
+    {'offset': '+06:00', 'timezones': []},
+    {'offset': '+06:30', 'timezones': []},
+    {'offset': '+07:00', 'timezones': []},
+    {'offset': '+08:00', 'timezones': ["AWST"]},
+    {'offset': '+08:45', 'timezones': ["ACWST"]},
+    {'offset': '+09:00', 'timezones': []},
+    {'offset': '+09:30', 'timezones': ["ACST"]},
+    {'offset': '+10:00', 'timezones': ["AEST"]},
+    {'offset': '+10:30', 'timezones': ["ACDT", "LHST"]},
+    {'offset': '+11:00', 'timezones': ["AEDT", "LHDT"]},
+    {'offset': '+12:00', 'timezones': []},
+    {'offset': '+12:45', 'timezones': []},
+    {'offset': '+13:00', 'timezones': []},
+    {'offset': '+14:00', 'timezones': []}
+]
+
+function getTimeFromMins(totalMinutes) {
+    // totalMinutes can be negative
+    const absMinutes = Math.abs(totalMinutes);
+    const hours = Math.floor(absMinutes / 60);
+    const minutes = absMinutes % 60;
+    const sign = totalMinutes < 0 ? '-' : '+';
+    const hoursString = hours.toString().padStart(2, '0');
+    const minutesString = minutes.toString().padStart(2, '0');
+    return `${sign}${hoursString}:${minutesString}`
+}
+
+function getUserUTCOffset() {
+    const userTimezoneOffsetInMinutes = new Date().getTimezoneOffset() * -1;
+    return getTimeFromMins(userTimezoneOffsetInMinutes);
+}
+
+export const DateTimeWithTimezone = () => {
+    return (
+        <Wrapper>
+            <DatetimeInput type="datetime-local"/>
+            <TimezoneDropdown value={getUserUTCOffset()}>
+                {TIMEZONE_UTC_OFFSETS.map(({offset, timezones}) => {
+                    const timezoneString = timezones.length > 0 ? ` (${timezones.join(', ')})` : '';
+                    return <Select.Option label={`UTC${offset}${timezoneString}`} value={offset}/>
+                })}
+            </TimezoneDropdown>
+        </Wrapper>
+    );
+}
+
--- packages/my-splunk-app/src/main/webapp/pages/new_indicator/index.jsx
+++ packages/my-splunk-app/src/main/webapp/pages/new_indicator/index.jsx
@@ -6,13 +6,24 @@ import {getUserTheme} from '@splunk/splunk-utils/themes';
 import ControlGroup from '@splunk/react-ui/ControlGroup';
 import {v4 as uuidv4} from 'uuid';
 import Text from '@splunk/react-ui/Text';
+import TextArea from '@splunk/react-ui/TextArea';
+
 import PropTypes from 'prop-types';
+import Select from "@splunk/react-ui/Select";
+import Number from '@splunk/react-ui/Number';
+import Button from "@splunk/react-ui/Button";
+import {DateTimeWithTimezone} from "@splunk/my-react-component/src/DateTimeWithTimezone";
 import {StyledContainer, StyledGreeting} from './styles';
 
-
-function MyForm({initialIndicatorId, splunkFieldName, splunkFieldValue}) {
+function NewIndicatorForm({initialIndicatorId, splunkFieldName, splunkFieldValue}) {
     return (
         <form>
+            <ControlGroup label="Grouping ID">
+                <Select placeholder="Select…">
+                    <Select.Option label="Grouping A" value="1" />
+                    <Select.Option label="Grouping B" value="2" />
+                </Select>
+            </ControlGroup>
             <ControlGroup label="Indicator ID">
                 <Text canClear value={initialIndicatorId} />
             </ControlGroup>
@@ -22,25 +33,57 @@ function MyForm({initialIndicatorId, splunkFieldName, splunkFieldValue}) {
             <ControlGroup label="Splunk Field Value">
                 <Text canClear value={splunkFieldValue} />
             </ControlGroup>
+            <ControlGroup label="Name">
+                <Text canClear />
+            </ControlGroup>
+            <ControlGroup label="Description">
+                <TextArea />
+            </ControlGroup>
+            <ControlGroup label="STIX v2 Pattern">
+                <TextArea />
+            </ControlGroup>
+            <ControlGroup label="Confidence">
+                <Number defaultValue={100} max={100} min={0} step={1} />
+            </ControlGroup>
+            <ControlGroup label="TLP Rating">
+                <Select>
+                    <Select.Option label="RED" value="RED" />
+                    <Select.Option label="AMBER" value="AMBER" />
+                    <Select.Option label="GREEN" value="GREEN" />
+                    <Select.Option label="WHITE" value="WHITE" />
+                </Select>
+            </ControlGroup>
+            <ControlGroup label="Valid From">
+                <DateTimeWithTimezone />
+            </ControlGroup>
+            <ControlGroup>
+                <Button label="Submit" appearance="primary" onClick={() => console.log("Submitted")}/>
+            </ControlGroup>
+
         </form>
     );
 }
-MyForm.propTypes = {
+
+NewIndicatorForm.propTypes = {
     initialIndicatorId: PropTypes.string,
+    splunkFieldName: PropTypes.string,
+    splunkFieldValue: PropTypes.string,
 };
+
 function getUrlQueryParams() {
     const urlParams = new URLSearchParams(window.location.search);
     const splunkFieldName = urlParams.get('splunkFieldName');
     const splunkFieldValue = urlParams.get('splunkFieldValue');
     return {splunkFieldName, splunkFieldValue}
 }
+
 getUserTheme()
     .then((theme) => {
         const {splunkFieldName, splunkFieldValue} = getUrlQueryParams();
         layout(
             <StyledContainer>
                 <StyledGreeting>New Indicator of Compromise (IoC)</StyledGreeting>
-                <MyForm initialIndicatorId={`indicator--${uuidv4()}`}
+                <NewIndicatorForm initialIndicatorId={`indicator--${uuidv4()}`}
                         splunkFieldName={splunkFieldName}
                         splunkFieldValue={splunkFieldValue}
                 />
