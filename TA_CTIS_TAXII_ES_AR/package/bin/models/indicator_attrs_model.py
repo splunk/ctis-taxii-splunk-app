b96f06a57d695db245f67cfdf2885d82b3453266
Ben Liew <bliew@splunk.com>
2024-08-16 10:47:00 +1000

    WIP on Indicator model

--- TA_CTIS_TAXII_ES_AR/package/bin/models/indicator_attrs_model.py
+++ TA_CTIS_TAXII_ES_AR/package/bin/models/indicator_attrs_model.py
@@ -1,17 +1,28 @@
-from attrs import define, field
-from stix2 import Indicator as StixIndicator
+from datetime import datetime
 
+from attrs import define, field
+from cattr.gen import make_dict_structure_fn
+from cattrs import Converter
+from stix2 import Indicator as StixIndicator
 from stix2patterns.validator import validate as stix_validate
+
+from .tlp_v1 import TLPv1
+
+
+# This Indicator Model represents fields required to generate a STIX 2.1 Indicator object
+@define(slots=False, kw_only=True)
+class BaseModel:
+    key: str = None
+    user: str = None
 
 
 @define(slots=False, kw_only=True)
-class BaseModel:
-    _key: str = None
-    _user: str = None
+class BaseModelV1(BaseModel):
+    schema_version: int = field(default=1)
 
 
-def validate_stix_pattern(pattern: str):
-    _, errors = stix_validate(pattern, ret_errs=True)
+def validate_stix_pattern(instance, attribute, value: str):
+    _, errors = stix_validate(value, ret_errs=True)
     if errors:
         raise ValueError(f"Invalid STIX pattern: {errors}")
 
@@ -23,10 +34,58 @@ def validate_indicator_id(instance, attribute, value):
         raise ValueError(f"Invalid indicator_id: {e}")
 
 
-@define(slots=False)
-class IndicatorModel(BaseModel):
+def validate_confidence(instance, attribute, value: int):
+    if not 0 <= value <= 100:
+        raise ValueError("Confidence must be between 0 and 100")
+
+
+@define(slots=False, kw_only=True)
+class IndicatorModelV1(BaseModelV1):
     indicator_id: str = field(validator=[validate_indicator_id])
     grouping_id: str = field()
     splunk_field_name: str = field()
     splunk_field_value: str = field()
     name: str = field()
+    description: str = field()
+    stix_pattern: str = field(validator=[validate_stix_pattern])
+    tlp_v1_rating: TLPv1 = field()
+    valid_from: datetime = field()
+    confidence: int = field(validator=[validate_confidence])
+
+
+indicator_converter = Converter()
+
+
+def unstructure_datetime_hook(val: datetime) -> str:
+    """This hook will be registered for `datetime`s."""
+    return val.isoformat()
+
+
+def structure_datetime_hook(value, type) -> datetime:
+    """This hook will be registered for `datetime`s."""
+    return datetime.fromisoformat(value)
+
+
+def make_structure(cls):
+    default_structure = make_dict_structure_fn(cls, indicator_converter)
+
+    def custom(val, another_cls):
+        key = val.get("_key")
+        user = val.get("_user")
+        if key is not None:
+            val["key"] = key
+            del val["_key"]
+        if user is not None:
+            val["user"] = user
+            del val["_user"]
+        result = default_structure(val, another_cls)
+        return result
+
+    return custom
+
+
+indicator_converter.register_structure_hook_factory(lambda cls: issubclass(cls, IndicatorModelV1), make_structure)
+indicator_converter.register_structure_hook(datetime, structure_datetime_hook)
+indicator_converter.register_unstructure_hook(datetime, unstructure_datetime_hook)
+
+# TODO: create unstructure hook to handle _key and _user fields
--- TA_CTIS_TAXII_ES_AR/test/test_model_indicator_attrs.py
+++ TA_CTIS_TAXII_ES_AR/test/test_model_indicator_attrs.py
@@ -1,8 +1,9 @@
 from datetime import datetime
 
 import pytest
-from TA_CTIS_TAXII_ES_AR.package.bin.models.indicator_attrs_model import IndicatorModel
-from cattrs import structure, unstructure
+from TA_CTIS_TAXII_ES_AR.package.bin.models.indicator_attrs_model import indicator_converter, IndicatorModelV1
+from TA_CTIS_TAXII_ES_AR.package.bin.models.tlp_v1 import TLPv1
+from cattrs import ClassValidationError, transform_error
 
 SAMPLE_DICT = {
     "schema_version": 1,
@@ -19,29 +20,89 @@ SAMPLE_DICT = {
     "_user": "nobody",
     "_key": "66bd393930444c60800ab750"
 }
-SAMPLE_DICT_NO_SPLUNK_RESERVED_FIELDS = SAMPLE_DICT.copy()
-del SAMPLE_DICT_NO_SPLUNK_RESERVED_FIELDS["_user"]
-del SAMPLE_DICT_NO_SPLUNK_RESERVED_FIELDS["_key"]
 
+SAMPLE_DICT_NO_SPLUNK_RESERVED_FIELDS = {k: v for k, v in SAMPLE_DICT.items() if k not in ["_user", "_key"]}
+
+SAMPLE_INDICATOR_INSTANCE = IndicatorModelV1(
+    grouping_id="A",
+    indicator_id="indicator--e669f9b4-80b1-4e66-97f1-d00227ac6c59",
+    splunk_field_name="src_ip",
+    splunk_field_value="1.2.3.4",
+    name="name",
+    description="desc",
+    stix_pattern="[network-traffic:src_ref.type = 'ipv4-addr' AND network-traffic:src_ref.value = '1.2.3.4']",
+    confidence=42,
+    tlp_v1_rating=TLPv1.GREEN,
+    valid_from=datetime(2024, 8, 14, 23, 9, 21, 123456),
+    user="nobody",
+    key="66bd393930444c60800ab750"
+)
+
+def test_from_dict_missing_required_fields():
+    with pytest.raises(ClassValidationError) as exc_info:
+        indicator_converter.structure({"something": "else"}, IndicatorModelV1)
+    error_strings = transform_error(exc_info.value)
+    assert all(['required field missing' in x for x in error_strings])
 
 def test_from_valid_dict_with_splunk_reserved_fields():
     as_dict = SAMPLE_DICT
-    indicator = structure(as_dict, IndicatorModel)
-    assert indicator._key == "66bd393930444c60800ab750"
-    assert indicator._user == "nobody"
+    indicator = indicator_converter.structure(as_dict, IndicatorModelV1)
+    assert indicator.schema_version == 1
+    assert indicator.key == "66bd393930444c60800ab750"
+    assert indicator.user == "nobody"
+    assert indicator.name == "name"
+    assert indicator.description == "desc"
+    assert indicator.stix_pattern == "[network-traffic:src_ref.type = 'ipv4-addr' AND network-traffic:src_ref.value = '100.2.3.4']"
+    assert indicator.tlp_v1_rating == TLPv1.WHITE
+    assert indicator.confidence == 100
+    assert indicator.grouping_id == "A"
+    assert indicator.indicator_id == "indicator--e669f9b4-80b1-4e66-97f1-d00227ac6c59"
+    assert indicator.splunk_field_name == "src_ip"
+    assert indicator.splunk_field_value == "100.2.3.4"
+    assert indicator.valid_from == datetime(2024, 8, 14, 23, 9, 21, 290000)
 
 def test_from_valid_dict_without_splunk_reserved_fields():
     as_dict = SAMPLE_DICT_NO_SPLUNK_RESERVED_FIELDS
-    indicator = structure(as_dict, IndicatorModel)
-    assert indicator._key is None
-    assert indicator._user is None
+    indicator = indicator_converter.structure(as_dict, IndicatorModelV1)
+    assert indicator.key is None
+    assert indicator.user is None
+    assert indicator.grouping_id == "A"
+    assert indicator.splunk_field_name == "src_ip"
+    assert indicator.splunk_field_value == "100.2.3.4"
+
+def test_from_valid_dict_without_schema_version():
+    as_dict = SAMPLE_DICT
+    del as_dict["schema_version"]
+    indicator = indicator_converter.structure(as_dict, IndicatorModelV1)
+    assert indicator.schema_version == 1, "Should set default version as 1"
+    assert indicator.grouping_id == "A"
+    assert indicator.splunk_field_name == "src_ip"
+    assert indicator.splunk_field_value == "100.2.3.4"
+
+def test_to_dict():
+    indicator = SAMPLE_INDICATOR_INSTANCE
+    as_dict = indicator_converter.unstructure(indicator)
+    assert as_dict["schema_version"] == 1
+    assert as_dict["_key"] == "66bd393930444c60800ab750"
+    assert as_dict["_user"] == "nobody"
+    assert as_dict["name"] == "name"
+    assert as_dict["description"] == "desc"
+    assert as_dict["stix_pattern"] == "[network-traffic:src_ref.type = 'ipv4-addr' AND network-traffic:src_ref.value = '1.2.3.4']"
+    assert as_dict["tlp_v1_rating"] == "GREEN"
+    assert as_dict["confidence"] == 42
+    assert as_dict["grouping_id"] == "A"
+    assert as_dict["indicator_id"] == "indicator--e669f9b4-80b1-4e66-97f1-d00227ac6c59"
+    assert as_dict["splunk_field_name"] == "src_ip"
+    assert as_dict["splunk_field_value"] == "1.2.3.4"
+    assert as_dict["valid_from"] == "2024-08-14T23:09:21.123456"
+
 
 @pytest.mark.parametrize("indicator_id", ["aaa", "indicator--invalid"])
 def test_validate_indicator_id(indicator_id):
     as_dict = SAMPLE_DICT
     as_dict["indicator_id"] = indicator_id
     with pytest.raises(Exception) as exc_info:
-        structure(as_dict, IndicatorModel)
+        indicator_converter.structure(as_dict, IndicatorModelV1)
     exc_string = str(exc_info.value.exceptions)
     assert "Invalid indicator_id" in exc_string
 
