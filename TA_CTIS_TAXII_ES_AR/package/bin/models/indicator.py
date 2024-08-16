fef3bcadb361a4a155d9afa029b252e240553091
Ben Liew <bliew@splunk.com>
2024-08-16 13:44:57 +1000

    Indicator model: delete old dataclasses version

--- TA_CTIS_TAXII_ES_AR/package/bin/models/indicator.py
+++ TA_CTIS_TAXII_ES_AR/package/bin/models/indicator.py
@@ -1,68 +1,62 @@
-from dataclasses import dataclass, field
 from datetime import datetime
 
-from dataclasses_json import config, dataclass_json
-from marshmallow import fields, validate, ValidationError
-from stix2patterns.validator import validate as stix_validate
+from attrs import define, field
 from stix2 import Indicator as StixIndicator
-from .base import BaseModel
+from stix2patterns.validator import validate as stix_validate
+
+from .base import BaseModelV1, make_base_converter
 from .tlp_v1 import TLPv1
 
+"""
+# This Indicator Model represents fields required to generate a STIX 2.1 Indicator object
+"""
 
-def validate_stix_pattern(pattern: str):
-    _, errors = stix_validate(pattern, ret_errs=True)
+
+def validate_stix_pattern(instance, attribute, value: str):
+    _, errors = stix_validate(value, ret_errs=True)
     if errors:
-        raise ValidationError(f"Invalid STIX pattern: {errors}")
+        raise ValueError(f"Invalid STIX pattern: {errors}")
 
 
-def validate_indicator_id(indicator_id: str):
+def validate_indicator_id(instance, attribute, value):
     try:
-        StixIndicator(id=indicator_id, pattern_type="stix2", pattern="")
+        StixIndicator(id=value, pattern_type="stix2", pattern="")
     except Exception as e:
-        raise ValidationError(f"Invalid indicator_id: {e}")
+        raise ValueError(f"Invalid indicator_id: {e}")
 
 
-@dataclass_json
-@dataclass
-class IndicatorV1(BaseModel):
-    """
-    Indicator model to be stored in KVStore Collection record
-    """
-    indicator_id: str = field(
-        metadata=config(
-            mm_field=fields.String(validate=validate_indicator_id)
-        )
-    )
-    grouping_id: str
-    splunk_field_name: str
-    splunk_field_value: str
-    name: str
-    description: str
+def validate_confidence(instance, attribute, value: int):
+    if not 0 <= value <= 100:
+        raise ValueError("confidence must be between 0 and 100")
 
-    stix_pattern: str = field(
-        metadata=config(
-            mm_field=fields.String(validate=validate_stix_pattern)
-        )
-    )
 
-    confidence: int = field(
-        metadata=config(
-            mm_field=fields.Integer(validate=validate.Range(min=0, max=100))
-        )
-    )
+@define(slots=False, kw_only=True)
+class IndicatorModelV1(BaseModelV1):
+    indicator_id: str = field(validator=[validate_indicator_id])
+    grouping_id: str = field()
+    splunk_field_name: str = field()
+    splunk_field_value: str = field()
+    name: str = field()
+    description: str = field()
+    stix_pattern: str = field(validator=[validate_stix_pattern])
+    tlp_v1_rating: TLPv1 = field()
+    valid_from: datetime = field()
+    confidence: int = field(validator=[validate_confidence])
 
-    tlp_v1_rating: TLPv1
 
-    valid_from: datetime = field(
-        metadata=config(
-            encoder=datetime.isoformat,
-            decoder=datetime.fromisoformat,
-            mm_field=fields.DateTime(format='iso')
-        )
-    )
-    schema_version: int = field(
-        default=1,
-        metadata=config(
-            mm_field=fields.Integer(validate=validate.Equal(1))
-        )
-    )
+indicator_converter = make_base_converter()
+
+
+# TODO: These could probably be moved to base model class?
+def unstructure_datetime_hook(val: datetime) -> str:
+    """This hook will be registered for `datetime`s."""
+    return val.isoformat()
+
+
+def structure_datetime_hook(value, type) -> datetime:
+    """This hook will be registered for `datetime`s."""
+    return datetime.fromisoformat(value)
+
+
+indicator_converter.register_structure_hook(datetime, structure_datetime_hook)
+indicator_converter.register_unstructure_hook(datetime, unstructure_datetime_hook)
