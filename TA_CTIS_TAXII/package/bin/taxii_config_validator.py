b7f1f6cde7c894ea5fdd2329ae05508d924f2af5
Ben Liew <bliew@splunk.com>
2024-09-29 18:04:05 +1000

    feat: TAXII Config - change to accept API Root URL

--- TA_CTIS_TAXII/package/bin/taxii_config_validator.py
+++ TA_CTIS_TAXII/package/bin/taxii_config_validator.py
@@ -10,17 +10,19 @@ logger.setLevel(logging.INFO)
 
 
 # https://github.com/oasis-open/cti-taxii-client/tree/master
-def _validate_connection(server_base_url: str, username, password):
-    logger.info(f"Validating connection to {server_base_url} with username={username}")
+def _validate_connection(api_root_url: str, username, password):
+    logger.info(f"Validating connection to {api_root_url} with username={username}")
     try:
-        from taxii2client.v21 import Server
-        server = Server(url=server_base_url, user=username, password=password)
-        server_title = server.title
-        logger.info(f"Connected to TAXII server: {server_title}")
+        from taxii2client.v21 import ApiRoot
+        server = ApiRoot(url=api_root_url, user=username, password=password)
+        collections = server.collections
+        logger.info(f"Connection to TAXII server ({api_root_url}) successful. Collections: {collections}")
     except Exception as e:
-        logger.exception(f"Connection to {server_base_url} failed: {e}")
-        raise RestError(message=f"Connection to {server_base_url} failed: {e}", status=400)
+        logger.exception(f"Connection to {api_root_url} failed: {e}")
+        raise RestError(message=f"Connection to {api_root_url} failed: {e}", status=400)
 
+
+API_ROOT_URL = "api_root_url"
 
 class CustomConnectionValidator(AdminExternalHandler):
     def __init__(self, *args, **kwargs):
@@ -30,12 +32,12 @@ class CustomConnectionValidator(AdminExternalHandler):
         AdminExternalHandler.handleList(self, confInfo)
 
     def handleEdit(self, confInfo):
-        _validate_connection(server_base_url=self.payload.get("server_base_url"), username=self.payload.get("username"),
+        _validate_connection(api_root_url=self.payload.get(API_ROOT_URL), username=self.payload.get("username"),
                              password=self.payload.get("password"))
         AdminExternalHandler.handleEdit(self, confInfo)
 
     def handleCreate(self, confInfo):
-        _validate_connection(server_base_url=self.payload.get("server_base_url"), username=self.payload.get("username"),
+        _validate_connection(api_root_url=self.payload.get(API_ROOT_URL), username=self.payload.get("username"),
                              password=self.payload.get("password"))
         AdminExternalHandler.handleCreate(self, confInfo)
 
