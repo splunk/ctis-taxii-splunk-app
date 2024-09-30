7d7e2eb5e6276a50197b53de5d4dc621afc7a492
Ben Liew <bliew@splunk.com>
2024-09-30 20:14:47 +1000

    feat: Endpoint to list submissions

---
+++ TA_CTIS_TAXII/package/bin/rest_list_submissions.py
@@ -0,0 +1,27 @@
+import os
+import sys
+
+sys.stderr.write(f"original sys.path: {sys.path}\n")
+sys.path.insert(0, os.path.join(os.path.dirname(__file__)))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "lib")))
+sys.stderr.write(f"updated sys.path: {sys.path}\n")
+
+try:
+    from common import get_logger_for_script, AbstractRestHandler, NAMESPACE
+    from models import IndicatorModelV1, indicator_converter
+    from solnlib._utils import get_collection_data
+    import remote_pdb
+except ImportError as e:
+    sys.stderr.write(f"ImportError: {e}\n")
+    raise e
+
+logger = get_logger_for_script(__file__)
+
+
+class ListSubmissionsHandler(AbstractRestHandler):
+    def handle(self, input_json: dict, query_params: dict, session_key: str) -> dict:
+        return self.handle_query_collection(input_json=input_json, query_params=query_params, session_key=session_key,
+                                            collection_name="submissions")
+
+
+Handler = ListSubmissionsHandler(logger=logger).generate_splunk_server_class()
