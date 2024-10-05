fd727280b079223b89fc7aa4a5a8e833e93b5720
Ben Liew <bliew@splunk.com>
2024-10-05 19:35:42 +1000

    lint: Remove unused code for view single submission

--- packages/my-splunk-app/src/main/webapp/pages/submissions/ViewSubmissionRecord.jsx
+++ packages/my-splunk-app/src/main/webapp/pages/submissions/ViewSubmissionRecord.jsx
@@ -1,21 +1,9 @@
-import styled from "styled-components";
-import {getSubmission, useGetRecord} from "@splunk/my-react-component/src/ApiClient";
-import Loader from "@splunk/my-react-component/src/Loader";
-import Heading from "@splunk/react-ui/Heading";
-import {CustomControlGroup} from "@splunk/my-react-component/src/CustomControlGroup";
 import {SubmissionStatusChip} from "@splunk/my-react-component/src/SubmissionStatusChip";
 import React from "react";
 import Code from "@splunk/react-ui/Code";
 import CollapsiblePanel from '@splunk/react-ui/CollapsiblePanel';
 import moment from "moment";
 import {GroupingIdLink} from "@splunk/my-react-component/src/urls";
-
-const Container = styled.div`
-    max-width: 1000px;
-`;
-const StyledSection = styled.section`
-    max-width: 1000px;
-`;
 
 export const formatScheduledAt = (scheduledAt) => {
     const timestampFormatted = moment.utc(scheduledAt).format("YYYY-MM-DD HH:mm:ss");
@@ -34,41 +22,22 @@ export const SUBMISSION_MAPPING_OF_FIELD_NAME_TO_RENDER = {
     "Bundle JSON Sent": (record) => {
         if (record.bundle_json_sent === null) {
             return NO_CONTENT;
-        } 
-            const formattedCode = JSON.stringify(JSON.parse(record.bundle_json_sent), null, 2);
-            return <CollapsiblePanel title="Show JSON" defaultOpen={false}>
-                <Code language='json' value={formattedCode}/>
-            </CollapsiblePanel>
-        
+        }
+        const formattedCode = JSON.stringify(JSON.parse(record.bundle_json_sent), null, 2);
+        return <CollapsiblePanel title="Show JSON" defaultOpen={false}>
+            <Code language='json' value={formattedCode}/>
+        </CollapsiblePanel>
+
     },
     "Response from TAXII Server": (record) => {
         if (record.response_json === null) {
             return NO_CONTENT;
-        } 
-            return <CollapsiblePanel defaultOpen={false} title="Show Response">
-                <code>{record.response_json ?? NO_CONTENT}</code>
-            </CollapsiblePanel>;
-        
+        }
+        return <CollapsiblePanel defaultOpen={false} title="Show Response">
+            <code>{record.response_json ?? NO_CONTENT}</code>
+        </CollapsiblePanel>;
+
     },
     "Error Message": (record) => record.error_message ? <code>{record.error_message}</code> : NO_CONTENT,
 }
 
-export function ViewSubmissionRecord({submissionId}) {
-    const {error, record, loading} = useGetRecord({
-        restGetFunction: getSubmission,
-        restFunctionQueryArgs: {submissionId}
-    });
-    return (<Loader loading={loading} error={error}>
-        <Container>
-            <Heading level={1}>Submission</Heading>
-            {record &&
-                <StyledSection>
-                    {Object.entries(SUBMISSION_MAPPING_OF_FIELD_NAME_TO_RENDER).map(([label, value]) => (
-                        <CustomControlGroup key={label} labelWidth={180} label={label} value={value(record)}
-                                            readOnly/>
-                    ))}
-                </StyledSection>
-            }
-        </Container>
-    </Loader>);
-}
