2756e13079ff75b299d1ecf7e1cf1b4d80d89aee
Ben Liew <bliew@splunk.com>
2024-10-11 23:16:27 +1100

    chore: remove unused func

--- packages/my-splunk-app/src/main/webapp/common/utils.jsx
+++ packages/my-splunk-app/src/main/webapp/common/utils.jsx
@@ -1,18 +1,4 @@
 import {useEffect} from "react";
-import {useFormContext} from "react-hook-form";
-
-export const useFieldWatchesStateValue = ({fieldName, stateValue, onChangeHook}) => {
-    const formMethods = useFormContext();
-    const {setValue} = formMethods;
-    useEffect(() => {
-        if (setValue && stateValue) {
-            setValue(fieldName, stateValue, {shouldValidate: true});
-            if(onChangeHook) {
-                setTimeout(() => onChangeHook(stateValue), 100);
-            }
-        }
-    }, [setValue, fieldName, stateValue, onChangeHook]);
-}
 
 export const usePageTitle = (title) => {
     useEffect(() => {
