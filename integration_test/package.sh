49717cf05b78af4df6495f693697f0c6a04e0be5
Ben Liew <bliew@splunk.com>
2024-10-18 17:29:59 +1100

    feat: Split up packaging app from spinning up docker container

---
+++ integration_test/package.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+set -e
+
+basename_of_pwd=$(basename "$PWD")
+if [ "$basename_of_pwd" != "integration_test" ]; then
+  echo "This script must be run from inside the integration_test directory"
+  exit 1
+fi
+
+# Build UCC app
+cd ..
+./full-build.sh
+cd -
+
+# Clean existing apps
+echo "Cleaning existing apps"
+rm -f ./*.tar.gz
+
+# Package the UCC app
+ucc-gen package --path ../TA_CTIS_TAXII/output/TA_CTIS_TAXII
+path_to_app=$(find . -type f -name "TA_CTIS*.tar.gz")
+num_apps=$(echo "$path_to_app" | wc -l | sed 's/^[[:space:]]*//g')
+echo "Found $num_apps apps"
+if [ "$num_apps" -ne 1 ]; then
+  echo "Expected to find 1 app, found $num_apps"
+  exit 1
+fi
+
+echo "Renaming app to ctis.tar.gz"
+mv "$path_to_app" ./ctis.tar.gz
--- integration_test/spin-up-docker-splunk.sh
+++ integration_test/spin-up-docker-splunk.sh
@@ -1,30 +1,25 @@
 #!/usr/bin/env bash
 set -e
 
-# Assume this script is run from inside integration_test directory
-# TODO: check this?
-
-# Build UCC app
-cd ..
-./full-build.sh
-cd -
-
-# Clean existing apps
-echo "Cleaning existing apps"
-rm -f ./*.tar.gz
-
-# Package the UCC app
-ucc-gen package --path ../TA_CTIS_TAXII/output/TA_CTIS_TAXII
-path_to_app=$(find . -type f -name "TA_CTIS*.tar.gz")
-num_apps=$(echo $path_to_app | wc -l | sed 's/^[[:space:]]*//g')
-echo "Found $num_apps apps"
-if [ $num_apps -ne 1 ]; then
-  echo "Expected to find 1 app, found $num_apps"
-  exit 1
+# Check the env var SPLUNK_APP_FILENAME is set
+if [ -z "$SPLUNK_APP_FILENAME" ]; then
+    echo "Please set the SPLUNK_APP_FILENAME environment variable"
+    exit 1
+fi
+# Check the env var SPLUNK_PASSWORD is set
+if [ -z "$SPLUNK_PASSWORD" ]; then
+    echo "Please set the SPLUNK_PASSWORD environment variable"
+    exit 1
 fi
 
-echo "Renaming app to ctis.tar.gz"
-mv "$path_to_app" ./ctis.tar.gz
+RED='\033[1;33m'
+NC='\033[0m' # No Color
+
+printf "${RED}WARNING: Make sure to package and build the app before running this script!\n${NC}"
+echo "Make sure to check the timestamp of the app file"
+ls -l "$SPLUNK_APP_FILENAME"
+
+APP_NAME='TA_CTIS_TAXII'
 
 # Stop & Remove existing splunk-ctis container
 container_name="splunk-ctis"
@@ -32,7 +27,6 @@ docker ps -q --filter "name=$container_name" | xargs -r docker stop
 sleep 2
 docker ps -aq --filter "name=$container_name" | xargs -r docker rm
 
-SPLUNK_PASSWORD='helloWorld1!'
 # Run splunk docker with the app installed
 # Web port exposed on localhost:8002
 # Admin port exposed on localhost:8099
@@ -42,12 +36,12 @@ DOCKER_DEFAULT_PLATFORM=linux/amd64 docker run -d --rm --name splunk-ctis --host
   -e "SPLUNK_PASSWORD=$SPLUNK_PASSWORD" \
   -e 'SPLUNK_START_ARGS=--accept-license' \
   -v "$(pwd):/tmp/test" \
-  -e 'SPLUNK_APPS_URL=/tmp/test/ctis.tar.gz' \
+  -e "SPLUNK_APPS_URL=/tmp/test/$SPLUNK_APP_FILENAME" \
   -it splunk/splunk:latest
 
 
 function checkIfSplunkIsUp() {
-    if curl -k -u "admin:$SPLUNK_PASSWORD" 'https://localhost:8099/services/apps/local?output_mode=json' 2> /dev/null | jq ".entry[].name" | grep TA_CTIS_TAXII; then
+    if curl -k -u "admin:$SPLUNK_PASSWORD" 'https://localhost:8099/services/apps/local?output_mode=json' 2> /dev/null | jq ".entry[].name" | grep "$APP_NAME"; then
         echo "App is installed"
         return 0
     else
@@ -56,18 +50,27 @@ function checkIfSplunkIsUp() {
     fi
 }
 
+function checkApiEndpoint() {
+    echo "Hitting List Submissions API endpoint"
+    if curl -k -u "admin:$SPLUNK_PASSWORD" "https://localhost:8099/servicesNS/-/$APP_NAME/list-submissions?output_mode=json"; then
+        return 0
+    else
+        return 1
+    fi
+}
+
 # Wait for splunk to be up
 echo "Time is now $(date)"
 printf "Waiting for Splunk to be up..."
 # TODO: wait for app to be installed and ready, because the docker image initially starts with no apps installed
-NUM_ATTEMPTS=10
+NUM_ATTEMPTS=5
 while true; do
     for i in $(seq 1 $NUM_ATTEMPTS); do
         if checkIfSplunkIsUp; then
             echo
             date
             echo "Splunk is up and app is installed (Attempt $i)"
-            sleep 1
+            sleep 3
         else
             echo "Splunk is not up yet."
             break
@@ -78,4 +81,16 @@ while true; do
         break
     fi
     sleep 5
+done
+
+# Wait for one of our app's API endpoints to reachable
+# TODO: We might not need to check for the app installation above if we can just check for the API endpoint
+while true; do
+    if checkApiEndpoint; then
+        echo "API endpoint is up"
+        break
+    else
+        echo "API endpoint is not up yet."
+        sleep 5
+    fi
 done
