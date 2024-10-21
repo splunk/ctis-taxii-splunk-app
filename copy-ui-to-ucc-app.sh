fdf3d7fa4b421ee7a5899545e11f81046c1d57e5
Ben Liew <bliew@splunk.com>
2024-10-21 14:51:19 +1100

    fix(package): Debugging build script - use GNU sed syntax instead of BSD (mac-os)

--- copy-ui-to-ucc-app.sh
+++ copy-ui-to-ucc-app.sh
@@ -11,11 +11,11 @@ ls -l TA_CTIS_TAXII/package/default/data/ui/views
 echo "Listing files in templates"
 ls -l TA_CTIS_TAXII/package/appserver/templates
 
-exit 0 # TODO: remove this line (early exit) after testing
-
+# https://stackoverflow.com/a/43453459/23523267
+# Uses GNU sed syntax. For MacOS, use: https://formulae.brew.sh/formula/gnu-sed
 # replace 'my-splunk-app:' with 'TA_CTIS_TAXII:' in all xml views in TA_CTIS_TAXII/package/default/data/ui/views
 echo "Replacing app name in views XML files..."
-find TA_CTIS_TAXII/package/default/data/ui/views -type f -name "*.xml" -exec sed -i '' 's/my-splunk-app/TA_CTIS_TAXII/g' {} \;
+find TA_CTIS_TAXII/package/default/data/ui/views -type f -name "*.xml" -exec sed -i 's/my-splunk-app/TA_CTIS_TAXII/g' {} \;
 
 echo "Replacing app name in template HTML files..."
-find TA_CTIS_TAXII/package/appserver/templates -type f -name "*.html" -exec sed -i '' 's/my-splunk-app/TA_CTIS_TAXII/g' {} \;
+find TA_CTIS_TAXII/package/appserver/templates -type f -name "*.html" -exec sed -i 's/my-splunk-app/TA_CTIS_TAXII/g' {} \;
