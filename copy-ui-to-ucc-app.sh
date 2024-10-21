82dc6be7462572f2b951c05b2580e51dad7d3148
Ben Liew <bliew@splunk.com>
2024-10-21 13:02:13 +1100

    fix(package): Debugging build script

--- copy-ui-to-ucc-app.sh
+++ copy-ui-to-ucc-app.sh
@@ -5,6 +5,14 @@ rsync -avh --progress packages/my-splunk-app/stage/appserver/ TA_CTIS_TAXII/pack
 # copy my-splunk-app/stage/default/data/ui/views to TA_CTIS_TAXII/package/default/data/ui/views
 rsync -avh --progress packages/my-splunk-app/stage/default/data/ui/views/ TA_CTIS_TAXII/package/default/data/ui/views
 
+echo "Listing files in views"
+ls -l TA_CTIS_TAXII/package/default/data/ui/views
+
+echo "Listing files in templates"
+ls -l TA_CTIS_TAXII/package/appserver/templates
+
+exit 1
+
 # replace 'my-splunk-app:' with 'TA_CTIS_TAXII:' in all xml views in TA_CTIS_TAXII/package/default/data/ui/views
 echo "Replacing app name in views XML files..."
 find TA_CTIS_TAXII/package/default/data/ui/views -type f -name "*.xml" -exec sed -i '' 's/my-splunk-app/TA_CTIS_TAXII/g' {} \;
