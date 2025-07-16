09e7660f50cb67540507b110af2456a89bd607ff
Ben Liew <bliew@splunk.com>
2025-07-16 20:24:33 +1000

    fix: Update TLP 2.0 marking definition created timestamps to 6 decimal places for microseconds

--- TA_CTIS_TAXII/package/bin/models/tlp_v2.py
+++ TA_CTIS_TAXII/package/bin/models/tlp_v2.py
@@ -9,7 +9,7 @@ AMBER_STRICT_MARKING_DEFINITION = stix2.parse({
     "type": "marking-definition",
     "spec_version": "2.1",
     "id": "marking-definition--939a9414-2ddd-4d32-a0cd-375ea402b003",
-    "created": "2022-10-01T00:00:00.000Z",
+    "created": "2022-10-01T00:00:00.000000Z",
     "name": "TLP:AMBER+STRICT",
     "extensions": {
         "extension-definition--60a3c5c5-0d10-413e-aab3-9e08dde9e88d": {
@@ -23,7 +23,7 @@ AMBER_MARKING_DEFINITION = stix2.parse({
     "type": "marking-definition",
     "spec_version": "2.1",
     "id": "marking-definition--55d920b0-5e8b-4f79-9ee9-91f868d9b421",
-    "created": "2022-10-01T00:00:00.000Z",
+    "created": "2022-10-01T00:00:00.000000Z",
     "name": "TLP:AMBER",
     "extensions": {
         "extension-definition--60a3c5c5-0d10-413e-aab3-9e08dde9e88d": {
@@ -37,7 +37,7 @@ CLEAR_MARKING_DEFINITION = stix2.parse({
     "type": "marking-definition",
     "spec_version": "2.1",
     "id": "marking-definition--94868c89-83c2-464b-929b-a1a8aa3c8487",
-    "created": "2022-10-01T00:00:00.000Z",
+    "created": "2022-10-01T00:00:00.000000Z",
     "name": "TLP:CLEAR",
     "extensions": {
         "extension-definition--60a3c5c5-0d10-413e-aab3-9e08dde9e88d": {
@@ -50,7 +50,7 @@ GREEN_MARKING_DEFINITION = stix2.parse({
     "type": "marking-definition",
     "spec_version": "2.1",
     "id": "marking-definition--bab4a63c-aed9-4cf5-a766-dfca5abac2bb",
-    "created": "2022-10-01T00:00:00.000Z",
+    "created": "2022-10-01T00:00:00.000000Z",
     "name": "TLP:GREEN",
     "extensions": {
         "extension-definition--60a3c5c5-0d10-413e-aab3-9e08dde9e88d": {
@@ -64,7 +64,7 @@ RED_MARKING_DEFINITION = stix2.parse({
     "type": "marking-definition",
     "spec_version": "2.1",
     "id": "marking-definition--e828b379-4e03-4974-9ac4-e53a884c97c1",
-    "created": "2022-10-01T00:00:00.000Z",
+    "created": "2022-10-01T00:00:00.000000Z",
     "name": "TLP:RED",
     "extensions": {
         "extension-definition--60a3c5c5-0d10-413e-aab3-9e08dde9e88d": {
