90375b7949e44557d107faf9b635f6e7d5f1a474
Ben Liew <bliew@splunk.com>
2026-07-01 17:12:08 +1000

    feat: New page for sightings. In default.xml use anchors to pass in an 'action' query param to the page. Placeholder components for List, Create and Edit sightings

---
+++ splunkui/packages/my-splunk-app/src/main/webapp/pages/sightings/CreateSighting.jsx
@@ -0,0 +1,16 @@
+import { PageHeading, PageHeadingContainer } from '@splunk/my-page/src/PageHeading';
+import React from 'react';
+import { usePageTitle } from '../../common/utils';
+
+const PAGE_TITLE = 'New Sighting';
+export default function CreateSighting() {
+    usePageTitle(PAGE_TITLE);
+
+    return (
+        <div>
+            <PageHeadingContainer>
+                <PageHeading level={1}>{PAGE_TITLE}</PageHeading>
+            </PageHeadingContainer>
+        </div>
+    );
+}
---
+++ splunkui/packages/my-splunk-app/src/main/webapp/pages/sightings/index.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { AppContainer } from '@splunk/my-page/src/AppContainer';
+import { layoutWithTheme } from '../../common/theme';
+import { getUrlQueryParams } from '../../common/queryParams';
+import ListSightings from './ListSightings';
+import CreateSighting from './CreateSighting';
+import EditSighting from './EditSighting';
+
+function Router() {
+    const queryParams = getUrlQueryParams();
+    const sightingId = queryParams.get('sighting_id')
+    let component = <ListSightings />;
+    if (queryParams.has('action', 'create')) {
+        component = <CreateSighting />;
+    }else if(queryParams.has('action', 'edit')){
+        component = <EditSighting sightingId={sightingId} />;
+    }
+    return component;
+}
+
+layoutWithTheme(
+    <AppContainer>
+        <Router />
+    </AppContainer>,
+);
