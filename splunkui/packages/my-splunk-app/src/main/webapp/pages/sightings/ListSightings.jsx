409a64c92f1fd460c014e25e252b483b10a92b9e
Ben Liew <bliew@splunk.com>
2026-07-02 16:12:49 +1000

    feat: Sightings page - minimal fields paginated table

--- splunkui/packages/my-splunk-app/src/main/webapp/pages/sightings/ListSightings.jsx
+++ splunkui/packages/my-splunk-app/src/main/webapp/pages/sightings/ListSightings.jsx
@@ -1,26 +1,27 @@
 import { PageHeading, PageHeadingContainer } from '@splunk/my-page/src/PageHeading';
-import React, { useContext, useState } from 'react';
+import React, { useState } from 'react';
 import { SightingsSearchBar } from '@splunk/my-page/src/SearchBar';
-import { PaginatedRecords, RecordsLoaderContext } from '@splunk/my-page/src/PaginatedDataTable';
+import { PaginatedRecords } from '@splunk/my-page/src/PaginatedDataTable';
 import { getSightings } from '@splunk/my-page/src/ApiClient';
 import { createErrorToast } from '@splunk/my-page/src/AppContainer';
+import { DataTableV2 } from '@splunk/my-page/src/ExpandableDataTable';
+import { formatTimestampForDisplay } from '@splunk/my-page/src/date_utils';
+import { IndicatorIdLink } from '@splunk/my-page/src/urls';
 import { usePageTitle } from '../../common/utils';
 
 const PAGE_TITLE = 'Sightings';
 
-function RenderDataTable() {
-    const {loading, error, records} = useContext(RecordsLoaderContext);
-    if (error){
-        return <div>ERROR: {JSON.stringify(error)}</div>
-    }
-    if (loading) {
-        return <div>Loading...</div>;
-    }
-    return <div>
-        <code>{JSON.stringify(records, null, 2)}</code>
-    </div>
-
+const EXPANSION_ROW_FIELD_NAME_TO_CELL_VALUE = {
+    "Sighting ID": (row) => row.sighting_id,
+    "Sighting of Ref": (row) => <IndicatorIdLink indicatorId={row.sighting_of_ref} />,
+    "Created At (UTC)": (row) => formatTimestampForDisplay(row.created),
+    "Updated At (UTC)": (row) => formatTimestampForDisplay(row.updated),
 }
+
+const mappingOfColumnNameToCellValue = [
+    { columnName: 'Sighting ID', getCellContent: (row) => row.sighting_id },
+    { columnName: 'Sighting of Ref', getCellContent: (row) => row.sighting_of_ref },
+];
 
 export default function ListSightings() {
     usePageTitle(PAGE_TITLE);
@@ -32,9 +33,12 @@ export default function ListSightings() {
                 <PageHeading level={1}>{PAGE_TITLE}</PageHeading>
             </PageHeadingContainer>
             <SightingsSearchBar onQueryChange={setQuery} />
-            <code>{JSON.stringify(query, null, 2)}</code>
             <PaginatedRecords fetchData={getSightings} onError={createErrorToast} query={query}>
-                <RenderDataTable />
+                <DataTableV2 rowKeyFunction={(row) => row.sighting_id}
+                             expansionRowFieldNameToCellValue={EXPANSION_ROW_FIELD_NAME_TO_CELL_VALUE}
+                             mappingOfColumnNameToCellValue={mappingOfColumnNameToCellValue}
+
+                />
             </PaginatedRecords>
         </div>
     );
