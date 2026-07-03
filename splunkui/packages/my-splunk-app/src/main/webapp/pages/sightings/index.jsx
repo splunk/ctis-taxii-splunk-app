57acc0288bc5b90fbd5efc9ce85e27c2f9fdbc67
Ben Liew <bliew@splunk.com>
2026-07-03 10:09:49 +1000

    feat: Show warning on Sightings pages if Sightings feature flag is disabled

--- splunkui/packages/my-splunk-app/src/main/webapp/pages/sightings/index.jsx
+++ splunkui/packages/my-splunk-app/src/main/webapp/pages/sightings/index.jsx
@@ -1,22 +1,42 @@
 import React from 'react';
 import { AppContainer } from '@splunk/my-page/src/AppContainer';
+import Loader from '@splunk/my-page/src/Loader';
+import {useAdvancedSettings} from '@splunk/my-page/src/hooks/useAdvancedSettings';
+import Layout from '@splunk/react-ui/Layout';
+import Message from '@splunk/react-ui/Message';
 import { layoutWithTheme } from '../../common/theme';
 import { getUrlQueryParams } from '../../common/queryParams';
 import ListSightings from './ListSightings';
 import CreateSighting from './CreateSighting';
 import EditSighting from './EditSighting';
 
-// TODO: Add feature flag check for enable_sightings
+function WarningSightingsFeatureDisabled(){
+    return (<Layout>
+        <Message appearance='fill' type='warning'>
+            <strong>Sightings Feature is not enabled.</strong>
+            <br />
+            If you would like to enable this feature navigate to Configuration &gt; Advanced Settings.
+            Then check the &#39;Enable Sightings Feature&#39; checkbox, and click the Save button.
+        </Message>
+    </Layout>);
+}
+
 function Router() {
+    const {loading, error, advancedSettings} = useAdvancedSettings();
     const queryParams = getUrlQueryParams();
     const sightingId = queryParams.get('sighting_id')
     let component = <ListSightings />;
-    if (queryParams.has('action', 'create')) {
+
+    if (!advancedSettings.enableSightings){
+        component = <WarningSightingsFeatureDisabled />;
+    }else if (queryParams.has('action', 'create')) {
         component = <CreateSighting />;
     }else if(queryParams.has('action', 'edit')){
         component = <EditSighting sightingId={sightingId} />;
     }
-    return component;
+    return (<Loader loading={loading} error={error}>
+        {component}
+    </Loader>)
 }
 
 layoutWithTheme(
