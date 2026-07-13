733ce0bb4da4cfd9647cb39ac2b46b39de29a7f0
Ben Liew <bliew@splunk.com>
2026-07-13 10:59:45 +1000

    feat: Sightings form - implement created_by_ref control

--- splunkui/packages/my-splunk-app/src/main/webapp/pages/sightings/form.jsx
+++ splunkui/packages/my-splunk-app/src/main/webapp/pages/sightings/form.jsx
@@ -13,7 +13,15 @@ import Button from '@splunk/react-ui/Button';
 import { viewSighting } from '@splunk/my-page/src/urls';
 import { usePageTitle } from '../../common/utils';
 import { ConfidenceField, FIELD_CONFIDENCE, FIELD_CONFIDENCE_OPTION } from '../../common/confidence';
-import { Count, DescriptionField, FirstSeen, LastSeen, SightingOfRef, WhereSightedRefs } from './formControls';
+import {
+    Count,
+    CreatedByRef,
+    DescriptionField,
+    FirstSeen,
+    LastSeen,
+    SightingOfRef,
+    WhereSightedRefs
+} from './formControls';
 import { useOnFormSubmit } from '../../common/formSubmit';
 
 const FORM_FIELD_SIGHTING_ID = 'sighting_id';
@@ -23,6 +31,7 @@ const FORM_FIELD_FIRST_SEEN = 'first_seen';
 const FORM_FIELD_LAST_SEEN = 'last_seen';
 const FORM_FIELD_COUNT = 'count';
 const FORM_FIELD_WHERE_SIGHTED_REFS = 'where_sighted_refs'
+const FORM_FIELD_CREATED_BY_REF = 'created_by_ref'
 
 
 const MyForm = styled.form`
@@ -67,6 +76,7 @@ export function Form({ existingSighting = null }) {
     register(FORM_FIELD_LAST_SEEN, { required: false, validate: validateFirstSeenIsBeforeLastSeen});
     register(FORM_FIELD_COUNT, { required: false, value: 1 });
     register(FORM_FIELD_WHERE_SIGHTED_REFS, { required: false, value: [] });
+    register(FORM_FIELD_CREATED_BY_REF, { required: false });
 
     useEffect(() => {
         if (existingSighting !== null) {
@@ -81,6 +91,7 @@ export function Form({ existingSighting = null }) {
             setValue(FORM_FIELD_LAST_SEEN, existingSighting[FORM_FIELD_LAST_SEEN])
             setValue(FORM_FIELD_COUNT, existingSighting[FORM_FIELD_COUNT])
             setValue(FORM_FIELD_WHERE_SIGHTED_REFS, existingSighting[FORM_FIELD_WHERE_SIGHTED_REFS]);
+            setValue(FORM_FIELD_CREATED_BY_REF, existingSighting[FORM_FIELD_CREATED_BY_REF]);
         }
     }, [existingSighting, register, setValue]);
 
@@ -125,6 +136,7 @@ export function Form({ existingSighting = null }) {
             <LastSeen fieldName={FORM_FIELD_LAST_SEEN}/>
             <Count fieldName={FORM_FIELD_COUNT}/>
             <WhereSightedRefs fieldName={FORM_FIELD_WHERE_SIGHTED_REFS}/>
+            <CreatedByRef fieldName={FORM_FIELD_CREATED_BY_REF}/>
             <CustomControlGroup>
                 <SubmitButton inline submitting={formState.isSubmitting} label='Submit' disabled={submitButtonDisabled}/>
             </CustomControlGroup>
