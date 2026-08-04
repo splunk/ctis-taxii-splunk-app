edc692d9552c70e9efa1db760799ecda66854a59
Ben Liew <bliew@splunk.com>
2026-08-04 17:18:16 +1000

    refactor: IndicatorsFileUploader extract reusable component

--- splunkui/packages/my-splunk-app/src/main/webapp/pages/bulk_import/IndicatorsFileUploader.jsx
+++ splunkui/packages/my-splunk-app/src/main/webapp/pages/bulk_import/IndicatorsFileUploader.jsx
@@ -1,10 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { PageHeading } from '@splunk/my-page/src/PageHeading';
 import Message from '@splunk/react-ui/Message';
 import PropTypes from 'prop-types';
 import FileUpload from './FileUpload';
 
-function extractIndicatorsFromBundleJSON(text) {
+function assertTextIsStixBundleJson(text){
     let obj;
     try {
         obj = JSON.parse(text);
@@ -14,12 +14,17 @@ function extractIndicatorsFromBundleJSON(text) {
     if (!Array.isArray(obj?.objects)) {
         throw new Error(`Expected "objects" to be a top-level array`);
     }
+}
+
+function extractIndicatorsFromBundleJSON(text) {
+    assertTextIsStixBundleJson(text);
+    const obj = JSON.parse(text);
     return obj.objects.filter(x => {
         return x?.type === 'indicator' && x?.spec_version === '2.1';
     });
 }
 
-export function IndicatorsFileUploader({ setIndicators, filename, setFilename }) {
+function GenericFileUploader({ onFileLoaded, filename, setFilename }) {
     const [fileText, setFileText] = useState(null);
     const [fileIsLoading, setFileIsLoading] = useState(false);
     const [errorMessage, setErrorMessage] = useState(null);
@@ -27,14 +32,14 @@ export function IndicatorsFileUploader({ setIndicators, filename, setFilename })
     useEffect(() => {
         if (fileText) {
             try {
-                setIndicators(extractIndicatorsFromBundleJSON(fileText));
+                console.log('Firing onFileLoaded')
+                onFileLoaded(fileText);
                 setErrorMessage(null);
             } catch (e) {
-                setIndicators([]);
                 setErrorMessage(e.message);
             }
         }
-    }, [fileText, setIndicators]);
+    }, [fileText, onFileLoaded]);
     return <>
         <PageHeading level={2}>Choose a JSON STIX Bundle file.</PageHeading>
         <FileUpload setFileText={setFileText} fileName={filename} setFileName={setFilename}
@@ -42,6 +47,25 @@ export function IndicatorsFileUploader({ setIndicators, filename, setFilename })
         {errorMessage && <Message type="error" appearance="fill">Error: {errorMessage}</Message>}
     </>;
 }
+
+GenericFileUploader.propTypes = {
+    onFileLoaded: PropTypes.func.isRequired,
+    filename: PropTypes.string.isRequired,
+    setFilename: PropTypes.func.isRequired,
+}
+
+export function IndicatorsFileUploader({ setIndicators, filename, setFilename }) {
+    const onFileLoaded = useCallback((fileText) => {
+        try {
+            setIndicators(extractIndicatorsFromBundleJSON(fileText));
+        } catch (e) {
+            setIndicators([]);
+        }
+    }, [setIndicators]);
+
+    return <GenericFileUploader onFileLoaded={onFileLoaded} filename={filename} setFilename={setFilename} />
+}
+
 IndicatorsFileUploader.propTypes = {
     setIndicators: PropTypes.func.isRequired,
     filename: PropTypes.string.isRequired,
