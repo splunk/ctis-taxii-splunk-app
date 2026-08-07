a04a2f78f50e1fb1ad83685ed97e7ad4fd371245
Ben Liew <bliew@splunk.com>
2026-08-07 16:32:33 +1000

    feat: Job Scheduler script - prepare to handle multiple types of jobs

--- TA_CTIS_TAXII/package/bin/job_scheduler.py
+++ TA_CTIS_TAXII/package/bin/job_scheduler.py
@@ -32,30 +32,40 @@ class MyHandler(AbstractRestHandler):
 @Configuration()
 class JobSchedulerCommand(GeneratingCommand):
 
-    def generate(self):
-        # To connect with Splunk, use the instantiated service object which is created using the server-uri and
-        # other meta details and can be accessed as shown below
-        # Example:-
-        #    service = self.service
-        service = self.service
-        session_key = service.token  # not sure if this is the right way to get the session key
-        handler = MyHandler()
-
-        # Need to manually set the kvstore_collections_context since we are not using the REST handler's infrastructure
-        handler.kvstore_collections_context = KVStoreCollectionsContext(session_key=session_key, app_namespace=NAMESPACE)
-
-        submissions = handler.kvstore_collections_context.submissions.list_scheduled_due_to_be_submitted()
+    def job_submission_scheduler(self):
+        submissions = self.handler.kvstore_collections_context.submissions.list_scheduled_due_to_be_submitted()
 
         logger.info(f"Number of submissions ready to be submitted: {len(submissions)}.")
 
         for submission in submissions:
             logger.info(f"Submission ready to be submitted: {submission}")
             try:
-                updated_submission = handler.submit_grouping(session_key=session_key,
+                updated_submission = self.handler.submit_grouping(session_key=self.service.token,
                                                              submission_id=submission.submission_id)
                 yield {'_time': time.time(), '_raw': json.dumps(updated_submission)}
             except Exception as exc:
                 logger.exception(f"Error submitting grouping: {exc}")
+
+
+    def generate(self):
+        # To connect with Splunk, use the instantiated service object which is created using the server-uri and
+        # other meta details and can be accessed as shown below
+        # Example:-
+        #    service = self.service
+        service = self.service
+        session_key = service.token
+
+        self.handler = MyHandler()
+
+        # Need to manually set the kvstore_collections_context since we are not using the REST handler's infrastructure
+        self.handler.kvstore_collections_context = KVStoreCollectionsContext(session_key=session_key, app_namespace=NAMESPACE)
+
+        for job_func in [self.job_submission_scheduler]:
+            try:
+                yield from job_func()
+            except Exception:
+                logger.exception("Error executing job")
+
 
 
 dispatch(JobSchedulerCommand, sys.argv, sys.stdin, sys.stdout, __name__)
